ba295201436d4f6d4e6a68c09c9c4e754ab850d4
Amean <amean.asad1999@gmail.com>
2022-02-02 09:10:16 -0500

    Meta changes for homepage

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <meta
                     name="description"
-                    content="Amean Asad personal website homepage"
+                    content="Amean Asad's personal website"
                 />
             </Head>
             <PageComponent pageId="home" Component={<HomePage />} />
